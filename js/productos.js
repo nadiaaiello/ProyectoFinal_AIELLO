8162b588cf031cb878149ca2ed036fff5d297039
nadiaaiello <nadialucianaaiello@gmail.com>
2023-07-17 16:16:43 -0300

    modal carrito

--- js/productos.js
+++ js/productos.js
@@ -1,7 +1,8 @@
 //CAPTURAR DOM
 let divProductos=document.getElementById("productos")
 let selectOrder=document.getElementById("selectOrden")
-
+let botonCarrito=document.getElementById("botonCarrito")
+let modalCarrito=document.getElementById("modal-bodyCarrito")
 
 //OBJETOS
 const bonObon={
@@ -46,66 +47,14 @@ const gaseosaCocaCola={
 
 }
 
+
+//ARRAYS
 const productos=[bonObon,chicleBubbaloo,chocolateAguila,gaseosaCocaCola,chupetinPop]
 
 const productosEnCarrito=[]
 
 
-const paqueteFamiliar={
-    nombre: "Paquete Familiar",
-    precio:100,
-    descripcion: "Incluye canasta, empapelado color verde y una carta personalizada."
-}
-
-const paqueteAmigos={
-    nombre: "Paquete Amigos",
-    precio:120,
-    descripcion: "Incluye caja cuadrada, empapelado color azul y un regalo sorpresa."
-}
-
-const paquetePareja={
-    nombre: "Paquete Pareja",
-    precio:150,
-    descripcion: "Incluye caja en forma de corazon, empapelado color rojo y una carta personalizada con fotos opcionales."
-}
-
-const paquetePropio={
-    nombre: "Paquete Auto-regalo",
-    precio:50,
-    descripcion: "Incluye bolsa, empapelado color violeta."
-}
-
-
 //FUNCTIONS
-function agregarAlCarrito(producto){
-    let productoAgregado = productosEnCarrito.find((elem)=>elem.id == producto.id) 
-    if(productoAgregado == undefined){
-       productosEnCarrito.push(producto)
-       localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
-       console.log(productosEnCarrito)
- 
-       //alert para agregar libro
-       Swal.fire({
-          title: `Ha agregado un producto al carrito`,
-          text:`${producto.nombre} ha sido agregado`,
-          confirmButtonColor: "green",
-          confirmButtonText : "Gracias :D",
-          imageUrl: `assets/${libro.imagen}`,
-          imageHeight: 200
- 
-       })
-    }else{
-        Swal.fire({
-           title: `El producto ya existe en el carrito`,
-           icon: "info",
-           //tiempo de aparición: en milisegundos
-           timer: 3000,
-           showConfirmButton: false
-  
-        })
-     }
-  }
-
 function ordenarAlfabeticamente() {   
 }
 
@@ -135,9 +84,9 @@ function printProductos(array){
 
 function agregarAlCarrito(elemento) {
         //preguntar si existe ese libro en el array
-        let libroAgregado = productosEnCarrito.find((elem)=>elem.id == elemento.id) 
+        let productoAgregado = productosEnCarrito.find((elem)=>elem.id == elemento.id) 
         //me devuelve sino encuentra undefined, si encuenta el elemento
-        if(libroAgregado == undefined){
+        if(productoAgregado == undefined){
            //código para sumar al array carrito
            productosEnCarrito.push(elemento)
            localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
@@ -152,27 +101,27 @@ function agregarAlCarrito(elemento) {
 
            
         }else{
-           //sumar uno a cantidad
-           // console.log(`El libro ${libro.titulo} ya existe en el carrito `)
-     
-           //Sweetalert 
-           Swal.fire({
-              title: `El libro ya existe en el carrito`,
-              icon: "info",
-              //tiempo de aparición: en milisegundos
-              timer: 2000,
-              showConfirmButton: false
-     
-           })
+           alert("El producto ya existe en el carrito")
         }
      }
 
 
+function verCarrito(array){
+   modalCarrito.innerHTML = ``
+   for(let elemento of array){
+    let nuevoProducto=document.createElement("div")
+    nuevoProducto.className="col-9 col-lg-4"
+    nuevoProducto.innerHTML=`<div class="card" id="${elemento.id}">
+                            <img src="../img/productos/${elemento.img}">
+                            <div class="card-body">
+                            <h4>${elemento.nombre}</h4>
+                            <p>${elemento.descripcion}<p/>
+                            <b class="d-inline me-3">$${elemento.precio}</b>
+                            <button id='agregarBtn${elemento.id}'><i class="bi bi-cart4"></i>Agregar al carrito</button>
+                            </div>
+                        </div>`
 
-function agregarProducto(){
-
-
-}
+modalCarrito.appendChild(nuevoProducto)}}
 
 function ordenarMenorMayor(array) {
     const menorMayor= [].concat(array)
@@ -233,3 +182,5 @@ selectOrden.addEventListener("change",()=>{
 }
 
 )
+
+botonCarrito.addEventListener("click",verCarrito(productosEnCarrito))
